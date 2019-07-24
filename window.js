09077d5170fafda0a4b8c8310633f9443528b2d9
tkreind <tkreind@umich.edu>
2019-07-23 21:53:28 -0400

    Data correctly submits to main.js

--- window.js
+++ window.js
@@ -1,8 +1,27 @@
 const { ipcRenderer } = require('electron');
 
-var form=document.getElementById("arguments");
+var form = document.getElementById("arguments");
 form.addEventListener("submit",submit,false);
 
 function submit() {
-	ipcRenderer.send('submitForm', "test");
+	var form = document.getElementById("arguments");
+	var data = {
+		program: form["-program"].value,
+		path: form["-path"].value,
+		xy: form["-xy"].value,
+		z: form["-z"].value,
+		scales: {
+			a: form["-scales-a"].value,
+			b: form["-scales-b"].value,
+			c: form["-scales-c"].value
+		},
+		threshold: form["-threshold"].value,
+		adaptive: form["-adaptive"].value,
+		binary: form["-binary"].value,
+		vtk: form["-vtk"].value,
+		labels_off: form["-labels_off"].value,
+		analyze: form["-analyze"].value,
+
+	}
+	ipcRenderer.send('submitForm', data);
 }
